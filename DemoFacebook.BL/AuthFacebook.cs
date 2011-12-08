1ae97dd1155439f923169eed961faea42125d243
Rodolfo Fadino <rodolfo.fadino@gmail.com>
2011-12-07 22:29:05 -0200

    criada a base para a classe AuthFacebook.cs

---
+++ DemoFacebook.BL/AuthFacebook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace DemoFacebook.BL
+{
+    public class AuthFacebook
+    {
+        public enum Method { GET, POST };
+        public const string Authorize = "";
+        public const string Access_Token = "";
+        public string CallBack_Url = "";
+
+        public string AplicationKey { get; set; }
+        public string AplicationSecret { get; set; }
+
+        public string Token{ get; set; }
+
+        public string GetAuthorizationLink()
+        {
+            return "";
+        }
+
+        public string GetAuthorizationLinkPopup()
+        {
+            return "";
+        }
+
+        public void GetAccessToken(string authToken)
+        {
+
+        }
+
+        public string Request(Method method, string url, string postData)
+        {
+            return "";
+        }
+
+        public  string GetWebResponse(HttpWebRequest webRequest)
+        {
+            return "";
+        }
+    }
+}
